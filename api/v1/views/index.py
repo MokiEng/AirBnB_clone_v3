d5e34203ec307123c989bb3fc0b352e4c220510b
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-28 04:31:42 -0700

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-'''Contains the index view for the AP.'''
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models import storage
-from api.v1.views import app_views
+'''Contains the index view for the API.'''
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns JSON """
-    return jsonify({"status": "OK"})
+    return jsonify(status="OK")
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def number_objects():
-    """returns the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    num_objs = {}
-    for j in range(len(classes)):
-        num_objs[names[j]] = storage.count(classes[j])
-
-    return jsonify(num_objs)
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
