ddd202bcd26031aed09301647b6df165c3d6559c
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-29 07:12:05 -0700

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -73,7 +73,7 @@ class FileStorage:
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
         except:
-            pass
+                   pass
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
