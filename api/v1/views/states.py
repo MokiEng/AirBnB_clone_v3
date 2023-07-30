d16c78de93f66bf919b328e183147645bbb2e703
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-30 05:17:01 -0700

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,85 +1,70 @@
 #!/usr/bin/python3
-""" Contains the states view for the API. """
+"""
+a new view for State objects that handles all default RESTFul API actions:
+"""
 
 from flask import abort, jsonify, make_response, request
-from models.state import State
-from models import storage
 from api.v1.views import app_views
-from flasgger.utils import swag_from
+from models import storage
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_state.yml', methods=['GET'])
-def get_states():
-    """Retrieves the list of all State objects."""
-    all_states = storage.all(State).values()
-    list_states = []
-    for state in all_states:
-        list_states.append(state.to_dict())
-    return jsonify(list_states)
+def state():
+    '''Retrieves the list of all State objects'''
+    objs = storage.all(State)
+    return jsonify([obj.to_dict() for obj in objs.values()])
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/state/get_id_state.yml', methods=['get'])
-def get_state(state_id):
-    """ Retrieves a State object """
-    state = storage.get(State, state_id)
-    if not state:
+def single_state(state_id):
+    '''Retrieves a State object'''
+    obj = storage.get(State, state_id)
+    if not obj:
         abort(404)
+    return jsonify(obj.to_dict())
 
-    return jsonify(state.to_dict())
 
-
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
-@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
-def delete_state(state_id):
-    """Deletes a State Object."""
-
-    state = storage.get(State, state_id)
-
-    if not state:
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def del_state(state_id):
+    '''Deletes a State object'''
+    obj = storage.get(State, state_id)
+    if not obj:
         abort(404)
-
-    storage.delete(state)
+    obj.delete()
     storage.save()
-
     return make_response(jsonify({}), 200)
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/state/post_state.yml', methods=['POST'])
 def post_state():
     """Returns the new State with the status code 201"""
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
-
-    data = request.get_json()
-    instance = State(**data)
-    instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    new_obj = request.get_json()
+    if not new_obj:
+        abort(400, "Not a JSON")
+    if 'name' not in new_obj:
+        abort(400, "Missing name")
+    obj = State(**new_obj)
+    storage.new(obj)
+    storage.save()
+    return make_response(jsonify(obj.to_dict()), 201)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/state/put_state.yml', methods=['PUT'])
 def put_state(state_id):
-    """Updates a State object."""
-    state = storage.get(State, state_id)
-
-    if not state:
+    """ Updates a State object """
+    obj = storage.get(State, state_id)
+    if not obj:
         abort(404)
 
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+    req = request.get_json()
+    if not req:
+        abort(400, "Not a JSON")
 
-    ignore = ['id', 'created_at', 'updated_at']
+    for key, value in req.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(obj, key, value)
 
-    data = request.get_json()
-    for k, v in data.items():
-        if k not in ignore:
-            setattr(state, k, v)
     storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+    return make_response(jsonify(obj.to_dict()), 200)
