fbe66846c021d8f1ba3c0bac128a0b1408bfcbf0
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-30 05:20:15 -0700

    Update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -2,86 +2,69 @@
 """a new view for User object that handles all default RESTFul API actions:
 """
 from flask import abort, jsonify, make_response, request
-from models.user import User
-from models import storage
 from api.v1.views import app_views
-from flasgger.utils import swag_from
+from models import storage
+from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/user/all_users.yml')
-def get_users():
-    """Retrieves the list of all User objects."""
-    all_users = storage.all(User).values()
-    list_users = []
-    for user in all_users:
-        list_users.append(user.to_dict())
-    return jsonify(list_users)
+def users():
+    '''Retrieves the list of all User objects'''
+    objs = storage.all(User)
+    return jsonify([obj.to_dict() for obj in objs.values()])
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/user/get_user.yml', methods=['GET'])
-def get_user(user_id):
-    """Retrieves a User object """
-    user = storage.get(User, user_id)
-    if not user:
+def single_user(user_id):
+    '''Retrieves a User object.'''
+    obj = storage.get(User, user_id)
+    if not obj:
         abort(404)
+    return jsonify(obj.to_dict())
 
-    return jsonify(user.to_dict())
 
-
-@app_views.route('/users/<user_id>', methods=['DELETE'],
-                 strict_slashes=False)
-@swag_from('documentation/user/delete_user.yml', methods=['DELETE'])
-def delete_user(user_id):
-    """Deletes a User object."""
-
-    user = storage.get(User, user_id)
-
-    if not user:
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def del_user(user_id):
+    """Deletes a User object"""
+    obj = storage.get(User, user_id)
+    if not obj:
         abort(404)
-
-    storage.delete(user)
+    obj.delete()
     storage.save()
-
     return make_response(jsonify({}), 200)
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/user/post_user.yml', methods=['POST'])
 def post_user():
-    """Creates a User."""
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+    """Creates a User"""
+    new_user = request.get_json()
+    if not new_user:
+        abort(400, "Not a JSON")
+    if 'email' not in new_user:
+        abort(400, "Missing email")
+    if 'password' not in new_user:
+        abort(400, 'Missing password')
 
-    if 'email' not in request.get_json():
-        abort(400, description="Missing email")
-    if 'password' not in request.get_json():
-        abort(400, description="Missing password")
-
-    data = request.get_json()
-    instance = User(**data)
-    instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    obj = User(**new_user)
+    storage.new(obj)
+    storage.save()
+    return make_response(jsonify(obj.to_dict()), 201)
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/user/put_user.yml', methods=['PUT'])
 def put_user(user_id):
-    """Updates a User object."""
-    user = storage.get(User, user_id)
-
-    if not user:
+    """Updates a User object"""
+    obj = storage.get(User, user_id)
+    if not obj:
         abort(404)
 
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+    req = request.get_json()
+    if not req:
+        abort(400, "Not a JSON")
 
-    ignore = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in req.items():
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(obj, key, value)
 
-    data = request.get_json()
-    for k, v in data.items():
-        if k not in ignore:
-            setattr(user, k, v)
     storage.save()
-    return make_response(jsonify(user.to_dict()), 200)
+    return make_response(jsonify(obj.to_dict()), 200)
